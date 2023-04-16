4131087ec55740165aed7368f269fa86e6f5ee23
JulyDiv <july_developer@list.ru>
2023-04-16 15:45:21 +0700

    сделала в форме инпуты

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import styles from "./Input.module.sass";
 
 export const Input = ({ label, type }) => {
+
   const [isActive, setIsActive] = useState(false);
+
   return (
     <>
       <div className={styles.form}>
